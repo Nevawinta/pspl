9bb079ce91e93bdaaba32091d9aef76262aed8a5
Andrew Zhao <azhao@dmail.caltech.edu>
2014-12-19 02:11:32 -0500

    Added code to support book view, cleaned up code a bit

--- data/preloader.js
+++ data/preloader.js
@@ -1,25 +1,43 @@
 console.log('onload called');   
+//this will preload the ith and after in stack view
 var obj = function(pix, i)
 {
     console.log('recursing number: ' + i);
-    if(i != pix.length)
+    if(i < pix.length)
     {
         pix[i].onload = function()
         {
             console.log('image onload called');
             obj(pix, i + 1);
-        }
+        };
+        pix[i].setAttribute('src', pix[i].getAttribute('data-src'));
     }
-    pix[i].setAttribute('src', pix[i].getAttribute('data-src'));
+};
+
+var preloadBook = function(pix, i)
+{
+    console.log('recursing number: ' + i);
+    if(i < pix.length)
+    {
+        var img = new Image();
+        img.onload = function()
+        {
+            console.log('image onload called');
+            preloadBook(pix, i + 1);
+        };
+        img.src = pix[i];
+    }
 }
 
-var images = document.getElementsByClassName('ui-scroll-view');
-console.log('starting jank recursion');
-obj(images, 0);
-/*
-for(var i = 0; i < pix.length; i++)
+//we check if we are in "book" view or stack view
+if(unsafeWindow.pixiv.context.images)
 {
-    var x = pix[i];
-    console.log('ng');
-    //x.setAttribute('src', x.getAttribute('data-src'));
-}*/
+    images = unsafeWindow.pixiv.context.images;
+    preloadBook(images, 0);
+}
+else
+{
+    var images = document.getElementsByClassName('ui-scroll-view');
+    console.log('starting jank recursion');
+    obj(images, 0);
+}
