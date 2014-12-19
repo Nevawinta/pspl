5da3f100f352b44e8561baf19090dbcf59dde4ed
Andrew Zhao <azhao@dmail.caltech.edu>
2014-12-19 02:33:06 -0500

    Inserted image elements as dom so the data is never uncached cleaned up issues with code

--- data/preloader.js
+++ data/preloader.js
@@ -1,31 +1,29 @@
-console.log('onload called');   
 //this will preload the ith and after in stack view
 var obj = function(pix, i)
 {
-    console.log('recursing number: ' + i);
     if(i < pix.length)
     {
-        pix[i].onload = function()
+        pix[i].addEventListener('load', function()
         {
-            console.log('image onload called');
             obj(pix, i + 1);
-        };
+        });
         pix[i].setAttribute('src', pix[i].getAttribute('data-src'));
     }
 };
 
 var preloadBook = function(pix, i)
 {
-    console.log('recursing number: ' + i);
     if(i < pix.length)
     {
         var img = new Image();
         img.onload = function()
         {
-            console.log('image onload called');
             preloadBook(pix, i + 1);
         };
         img.src = pix[i];
+        //insert it into the page hidden so the data stays cached
+        img.style.display = 'none';
+        document.body.appendChild(img);
     }
 }
 
@@ -38,6 +36,5 @@ if(unsafeWindow.pixiv.context.images)
 else
 {
     var images = document.getElementsByClassName('ui-scroll-view');
-    console.log('starting jank recursion');
     obj(images, 0);
 }
